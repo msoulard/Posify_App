200bb1a3f38d126bfe27d318b1c0369c55ca160e
Maëva Soulard <58474758+msoulard@users.noreply.github.com>
2023-02-09 16:25:35 +0100

    création de la struture de la bdd en sql

---
+++ BDD/StructureBDD_Posify.sql
@@ -0,0 +1,234 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le : jeu. 09 fév. 2023 à 15:23
+-- Version du serveur :  10.4.12-MariaDB-log
+-- Version de PHP : 7.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `applicationPosify`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `application`
+--
+
+CREATE TABLE `application` (
+  `id_application` int(11) NOT NULL,
+  `nom_application` varchar(50) NOT NULL,
+  `logo_application` varchar(500) DEFAULT NULL,
+  `version_application` varchar(10) NOT NULL,
+  `langue_defaut_application` varchar(20) NOT NULL,
+  `date_creation_application` date NOT NULL,
+  `date_dernierModification_application` date DEFAULT NULL,
+  `conditions_utilisation_application` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `localisation`
+--
+
+CREATE TABLE `localisation` (
+  `id_localisation` int(11) NOT NULL,
+  `date_localisation` date NOT NULL,
+  `nom_lieu_localisation` varchar(20) NOT NULL,
+  `id_personne` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `notification`
+--
+
+CREATE TABLE `notification` (
+  `id_notification` int(11) NOT NULL,
+  `date_notification` date NOT NULL,
+  `description_notification` varchar(150) NOT NULL,
+  `id_personne` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `personne`
+--
+
+CREATE TABLE `personne` (
+  `id_personne` int(11) NOT NULL,
+  `nom_personne` varchar(50) NOT NULL,
+  `prenom_personne` varchar(50) NOT NULL,
+  `avatar_personne` varchar(500) DEFAULT NULL,
+  `mdp_personne` varchar(50) NOT NULL,
+  `identifiant_connexion_personne` varchar(50) NOT NULL,
+  `adresse_mail_personne` varchar(100) DEFAULT NULL,
+  `numero_carteUniv_personne` varchar(25) DEFAULT NULL,
+  `statut_personne` enum('etudiant','professeur','administration','visiteur') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `recherche`
+--
+
+CREATE TABLE `recherche` (
+  `id_recherche` int(11) NOT NULL,
+  `date_recherche` date NOT NULL,
+  `id_personne` int(11) NOT NULL,
+  `id_personneRecherchee` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisationApplication`
+--
+
+CREATE TABLE `utilisationApplication` (
+  `id_utilisationApplication` int(11) NOT NULL,
+  `langue_utilisee` varchar(20) DEFAULT NULL,
+  `localisation_active` tinyint(1) DEFAULT NULL,
+  `condition_acceptee` tinyint(1) DEFAULT NULL,
+  `connexion_active` tinyint(1) DEFAULT NULL,
+  `id_personne` int(11) NOT NULL,
+  `id_application` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `application`
+--
+ALTER TABLE `application`
+  ADD PRIMARY KEY (`id_application`);
+
+--
+-- Index pour la table `localisation`
+--
+ALTER TABLE `localisation`
+  ADD PRIMARY KEY (`id_localisation`),
+  ADD KEY `id_personne` (`id_personne`);
+
+--
+-- Index pour la table `notification`
+--
+ALTER TABLE `notification`
+  ADD PRIMARY KEY (`id_notification`),
+  ADD KEY `id_personne` (`id_personne`);
+
+--
+-- Index pour la table `personne`
+--
+ALTER TABLE `personne`
+  ADD PRIMARY KEY (`id_personne`);
+
+--
+-- Index pour la table `recherche`
+--
+ALTER TABLE `recherche`
+  ADD PRIMARY KEY (`id_recherche`),
+  ADD KEY `id_personne` (`id_personne`),
+  ADD KEY `id_personneRecherchee` (`id_personneRecherchee`);
+
+--
+-- Index pour la table `utilisationApplication`
+--
+ALTER TABLE `utilisationApplication`
+  ADD PRIMARY KEY (`id_utilisationApplication`),
+  ADD KEY `id_personne` (`id_personne`),
+  ADD KEY `id_application` (`id_application`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `application`
+--
+ALTER TABLE `application`
+  MODIFY `id_application` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `localisation`
+--
+ALTER TABLE `localisation`
+  MODIFY `id_localisation` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `notification`
+--
+ALTER TABLE `notification`
+  MODIFY `id_notification` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `personne`
+--
+ALTER TABLE `personne`
+  MODIFY `id_personne` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `recherche`
+--
+ALTER TABLE `recherche`
+  MODIFY `id_recherche` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `utilisationApplication`
+--
+ALTER TABLE `utilisationApplication`
+  MODIFY `id_utilisationApplication` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `localisation`
+--
+ALTER TABLE `localisation`
+  ADD CONSTRAINT `localisation_ibfk_1` FOREIGN KEY (`id_personne`) REFERENCES `personne` (`id_personne`);
+
+--
+-- Contraintes pour la table `notification`
+--
+ALTER TABLE `notification`
+  ADD CONSTRAINT `notification_ibfk_1` FOREIGN KEY (`id_personne`) REFERENCES `personne` (`id_personne`);
+
+--
+-- Contraintes pour la table `recherche`
+--
+ALTER TABLE `recherche`
+  ADD CONSTRAINT `recherche_ibfk_1` FOREIGN KEY (`id_personne`) REFERENCES `personne` (`id_personne`),
+  ADD CONSTRAINT `recherche_ibfk_2` FOREIGN KEY (`id_personneRecherchee`) REFERENCES `personne` (`id_personne`);
+
+--
+-- Contraintes pour la table `utilisationApplication`
+--
+ALTER TABLE `utilisationApplication`
+  ADD CONSTRAINT `utilisationApplication_ibfk_1` FOREIGN KEY (`id_personne`) REFERENCES `personne` (`id_personne`),
+  ADD CONSTRAINT `utilisationApplication_ibfk_2` FOREIGN KEY (`id_application`) REFERENCES `application` (`id_application`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
